ae477ad85037a187e80e5c15d905f91ce985c58f
eddyeboy27 <lipe0503@gmail.com>
2021-04-10 19:11:24 -0300

    Refatorado codigo da função doc_reader

--- file_management.py
+++ file_management.py
@@ -1,12 +1,27 @@
-def doc_reader(path_file):
-    text_splited_bytes = list()
+def doc_reader(path_file, doc):
+    words_dict = dict()
+    counter = 0
     with open(path_file, 'rb') as text_doc:
-        text_lines = text_doc.read()
-        for line in text_lines.split(b" "):
-            line2 = line.split()
-            line3 = line.split(b'\n')
-            for word in line3:
-                word_decoded = word.decode('cp437')
-                if not word_decoded == '':
-                    text_splited_bytes.append(word_decoded)
-    return text_splited_bytes
+        text_lines = text_doc.read().split()
+        for line in text_lines:
+            word_decoded = line.decode('cp437').lower()
+            if not word_decoded == '':
+                words_dict[word_decoded] = counter
+                counter += 1
+    return words_dict
+
+
+def exist_word(path_file, word):
+    with open(path_file, 'rb') as text_doc:
+        text_list = list()
+        text_lines = text_doc.read().split()
+        for word in text_lines:
+            text_lines.append(word)
+        print(text_list)
+        # for line in text_lines.split(b" "):
+        #     line2 = line.split()
+        #     line3 = line.split(b'\n')
+        #     for wordl in line3:
+        #         word_decoded = wordl.decode('cp437').lower()
+        #         if ((not word_decoded == '') and (word_decoded == word)):
+        #             return True
