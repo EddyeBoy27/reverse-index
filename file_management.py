16f686071074eae0c6df81d0d53545fc737ef694
eddyeboy27 <lipe0503@gmail.com>
2021-04-10 22:30:27 -0300

    Refatorado código e finalizado desafio

--- file_management.py
+++ file_management.py
@@ -1,27 +1,9 @@
 def doc_reader(path_file, doc):
-    words_dict = dict()
-    counter = 0
+    words_list = list()
     with open(path_file, 'rb') as text_doc:
         text_lines = text_doc.read().split()
         for line in text_lines:
-            word_decoded = line.decode('cp437').lower()
+            word_decoded = line.decode('cp437')
             if not word_decoded == '':
-                words_dict[word_decoded] = counter
-                counter += 1
-    return words_dict
-
-
-def exist_word(path_file, word):
-    with open(path_file, 'rb') as text_doc:
-        text_list = list()
-        text_lines = text_doc.read().split()
-        for word in text_lines:
-            text_lines.append(word)
-        print(text_list)
-        # for line in text_lines.split(b" "):
-        #     line2 = line.split()
-        #     line3 = line.split(b'\n')
-        #     for wordl in line3:
-        #         word_decoded = wordl.decode('cp437').lower()
-        #         if ((not word_decoded == '') and (word_decoded == word)):
-        #             return True
+                words_list.append(word_decoded)
+    return words_list
--- process.py
+++ process.py
@@ -1,59 +1,23 @@
 import os
-from file_management import doc_reader, exist_word
+from file_management import doc_reader
 
 
 def reverse_index(dir_path):
-    # files_arr = sorted(os.listdir(dir_path), key=lambda x: x[:-1])
-    files_arr = ['0']
+    files_arr = sorted(os.listdir(dir_path), key=lambda x: x[:-1])
     words_list_global = dict()
-    words_list_set = set()
-    words_list = list()
-    reverse_index = dict()
-    # words_set_index = set()
-    words_dict_index = dict()
-    # words_ids = dict()
     counter = 0
     for doc in files_arr:
         doc_read = doc_reader(f"./dataset/{doc}", doc)
-        # for word in doc_read:
-        #     if not word in words_list_global:
-        #         words_list_global[word] = counter
-        #         counter += 1
-    # for doc in files_arr:
-    #     for word in words_list_global:
-    #         verify_word = exist_word(f"./dataset/{doc}", word)
-    #         if verify_word:
-    #             if not words_list_global[word] in reverse_index:
-    #                 reverse_index[words_list_global[word]] = list(doc)
-    #             else:
-    #                 reverse_index[words_list_global[word]].append(doc)
-    # print(reverse_index)
-    # for index, array in enumerate(words_list):
-    #     if array[0] in words_list:
-    #         words_list[index] = [array]
-    #         words_dict_index[word] = doc_read[word]
-    #     for word in doc_read:
-    #         if ((word in words_dict_index) & (doc_read[word] == words_dict_index[word])):
-    #             words_list_set.add((doc_read[word], [doc]))
-    # print(words_list_set)
-    # print(words_dict_index)
-            # words_list_set.add(word)
-    # print(words_list_set)
-            # words_list.add(word)
-            # counter += 1
-            # if not word in words_dict_index:
-            #     words_dict_index[word] = counter
-            #     words_list_set.add((word, counter, doc))
-            #     counter += 1
-        # print(words_set)
-    # for word in words_dict_index:
-    #     print(words_dict_index[word])
-    #     for value in words_dict_index.values():
-    #         if not value in words_ids.values():
-    #             words_ids[value] = list()
-    #         words_ids[value].append(doc)
-    # print(words_ids)
-    # print(words_dict_index)
+        for word in doc_read:
+            if not word in words_list_global:
+                words_list_global[word] = list()
+                words_list_global[word].append(counter)
+                words_list_global[word].append([])
+                words_list_global[word][1].append(doc)
+            if ((word in words_list_global) and (not doc in words_list_global[word][1])): 
+                words_list_global[word][1].append(doc)
+            counter += 1
+    return words_list_global
 
 
-reverse_index('./dataset/')
+print(reverse_index('./dataset/'))
